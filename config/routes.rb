2da7ce3f365da3ea9c3cb6eb2e377538a80fa864
Leonel Galan <leonelgalan@gmail.com>
2014-05-30 14:35:57 -0400

    Welcome#home action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 VagrantTest::Application.routes.draw do
+  get 'welcome/home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
